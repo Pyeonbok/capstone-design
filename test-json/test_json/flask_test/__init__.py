5e45910d2a859cc1f5f3b5106d18ec6f095ed348
Jiyeon-Kang <77331456+Jiyeon-Kang@users.noreply.github.com>
2021-03-18 18:48:45 +0900

    Add files via upload

--- test-json/test_json/flask_test/__init__.py
+++ test-json/test_json/flask_test/__init__.py
@@ -10,7 +10,7 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-# 할일 정의
+# json 파일 open
 with open('data.json', 'r', encoding='utf-8') as f:
     json_data = json.load(f)
 
@@ -21,7 +21,7 @@ jsob = json.loads(CLASSES)
 #     print(jsob[i].get("교과목명"))
 
 line = len(jsob)
-
+"""
 # print(jsonSubject.get("교과목명"))
 
 # print(CLASSES)
@@ -46,8 +46,6 @@ line = len(jsob)
 #         abort(404, message="Todo {} doesn't exist".format(todo_id))
 #
 #
-parser = reqparse.RequestParser()
-parser.add_argument('task')
 
 # # 할일(Todo)
 # # Get, Delete, Put 정의
@@ -66,45 +64,47 @@ parser.add_argument('task')
 #         task = {'task': args['task']}
 #         CLASSES[todo_id] = task
 #         return task, 201
+"""
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
-
-# 할일 리스트(Todos)
 # Get, POST 정의
 class TodoList(Resource):
-
+    
     def get(self, stt_text):
         print(stt_text)
         df = pd.read_csv('class_all_LAST.csv', encoding='utf-8')
-
-        #text = '예정용 교수님 교양 추천해줘'  # 안드로이드에서 받아올 텍스트
-        df.loc[(df['class'] == '검색'), 'keyword'] = stt_text # 검색을 받아온 텍스트로 함.
+        
+        df.loc[(df['class'] == '검색'), 'keyword'] = stt_text #csv파일의 class에서 검색 찾기
         # print(df)
 
         tfidf_vectorizer = TfidfVectorizer()  # TF-IDF 객체선언
-        tfidf_matrix = tfidf_vectorizer.fit_transform(df['keyword'])
+        tfidf_matrix = tfidf_vectorizer.fit_transform(df['keyword'])#tf-idf백터화
 
-        cosine_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
+        cosine_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)#코싸인 유사도 매트릭스화
 
         class2id = {}
-        for i, c in enumerate(df['class']): class2id[i] = c
+        for i, c in enumerate(df['class']): class2id[i] = c#class를 순서대로 id 붙이기
+        
         id2class = {}
-        for i, c in class2id.items(): id2class[c] = i
+        for i, c in class2id.items(): id2class[c] = i#붙인 id로 클래스 불러오기
+        
         class_Num={}
-        for i, c in enumerate(df['class_num']): class_Num[i] = c
-
+        for i, c in enumerate(df['class_num']): class_Num[i] = c#학수번호로 id 붙이기
+        
 
 
         idx = id2class['검색']
         #print(class2id)
         #print(id2class)
         print(class_Num)
-        sim_scores = [(i, c) for i, c in enumerate(cosine_matrix[idx]) if i != idx]  # 자기 자신을 제외한 영화들의 유사도 및 인덱스를 추출
+        sim_scores = [(i, c) for i, c in enumerate(cosine_matrix[idx]) if i != idx]  #받은 텍스트와 관련된 과목 유사도 및 인덱스를 추출
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)  # 유사도가 높은 순서대로 정렬
 
-        sim_scores = [(class_Num[i], score) for i, score in sim_scores[0:10]]
+        sim_scores = [(class_Num[i], score) for i, score in sim_scores[0:10]]#상위 10개의 유사도를 가진 과목 저장
         #print(sim_scores)
         res = []
-        for i in range(10):
+        for i in range(10):#10개 과목을 리스트 res에 저장
             if float(sim_scores[i][1])>0:
                 res.append(sim_scores[i][0])
 
@@ -114,17 +114,16 @@ class TodoList(Resource):
 
 
         #print(m)
+        #json 불러오기
         list = []
-        #코드 수정 필요
         for i in range(1,11):
             tmp = str(i)
             str2 = str(jsonObject.get(tmp))
             for i in range(1,line):
-                if str2 == jsob[i].get("학수번호"): # 비교해서 같을 경우
+                if str2 == jsob[i].get("학수번호"):#저장한 학수번호와 비교해서 같을 경우
                     str3 = jsob[i].get("교과목명"),jsob[i].get("담당교수"),jsob[i].get("시간표")
-                    # 전체정보출력가능
-                    list.append(str3)
-                    print(jsob[i].get("교과목명"),jsob[i].get("담당교수"),jsob[i].get("시간표"))
+                    list.append(str3)#교과목명 담당교수 수업시간을 list에 저장
+                    #print(jsob[i].get("교과목명"),jsob[i].get("담당교수"),jsob[i].get("시간표"))
 
 
         l = dict(zip(range(1,len(str3)+1), str3)) # 추출한 정보들을 json으로 변환.
